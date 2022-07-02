98c0250636e2cbae654c36ade7bf561e0c66a42b
Yegor Kharitonchik <egorkhary@mail.ru>
2022-07-02 20:22:18 +0300

    Add files via upload

---
+++ Booleans.cs
@@ -0,0 +1,17 @@
+﻿namespace Bools
+{
+    public static class Booleans
+    {
+        public static bool ReturnTrue()
+        {
+            // TODO #1-1. Return "true" literal.
+            return true;
+        }
+
+        public static bool ReturnFalse()
+        {
+            // TODO #1-2. Return "false" literal.
+            return false;
+        }
+    }
+}
